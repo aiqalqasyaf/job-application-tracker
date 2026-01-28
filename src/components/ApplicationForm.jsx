68a4f4068360617b836a843ea38a485d36da36bf
aiqalqasyaf <qasyaf@gmail.com>
2026-01-28 21:45:22 +0800

    add status system with dropdown and badges

--- src/components/ApplicationForm.jsx
+++ src/components/ApplicationForm.jsx
@@ -3,14 +3,16 @@ import "../styles/index.css";
 export default function ApplicationForm({
     company,
     role,
+    status,
     setCompany,
     setRole,
+    setStatus,
     onSubmit,
 }) {
     return (
         <form className="w-full flex justify-center gap-2" onSubmit={onSubmit}>
             <input
-                className="h-9 border-2 rounded-md px-3 py-1 transition duration-300 ease-in-out"
+                className="h-9 border-2 rounded-md px-3 py-1 hover:scale-101 transition duration-300 ease-in-out"
                 value={company}
                 onChange={(e) => setCompany(e.target.value)}
                 placeholder="Company"
@@ -23,6 +25,18 @@ export default function ApplicationForm({
                 placeholder="Role"
                 required
             />
+            <select
+                className="h-9 border-2 rounded-md px-3 py-1 hover:scale-101 transition duration-300 ease-in-out"
+                name="status"
+                id="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+            >
+                <option value="Applied">Applied</option>
+                <option value="Interviewing">Interviewing</option>
+                <option value="Offer">Offer</option>
+                <option value="Rejected">Rejected</option>
+            </select>
             <button
                 className="font-bold h-9 bg-black text-white py-1 px-3 rounded-lg transition duration-300 ease-in-out hover:scale-101 active:cursor-grab hover:cursor-pointer"
                 type="submit"
